f4fef3267503456061c638d25a8eca2c296712ba
Lifan <2224779926@qq.com>
2018-03-08 20:46:49 +0800

    实现了导航栏功能

--- app/build/generated/source/r/debug/com/example/life/R.java
+++ app/build/generated/source/r/debug/com/example/life/R.java
@@ -8,25 +8,70 @@
 package com.example.life;
 
 public final class R {
+  public static final class color {
+    public static final int text_blue=0x7f010000;
+    public static final int text_gray=0x7f010001;
+    public static final int text_red=0x7f010002;
+    public static final int text_white=0x7f010003;
+    public static final int transparent=0x7f010004;
+  }
   public static final class drawable {
-    public static final int ic_la=0x7f010000;
-    public static final int ic_launcher=0x7f010001;
-    public static final int s_activity=0x7f010002;
+    public static final int bg_num=0x7f020000;
+    public static final int ic_la=0x7f020001;
+    public static final int ic_launcher=0x7f020002;
+    public static final int ic_menu_deal_off=0x7f020003;
+    public static final int ic_menu_deal_on=0x7f020004;
+    public static final int ic_menu_meg_off=0x7f020005;
+    public static final int ic_menu_meg_on=0x7f020006;
+    public static final int ic_menu_more_on=0x7f020007;
+    public static final int ic_menu_user_off=0x7f020008;
+    public static final int ic_menu_user_on=0x7f020009;
+    public static final int s_activity=0x7f02000a;
+    public static final int tab_menu_better=0x7f02000b;
+    public static final int tab_menu_bg=0x7f02000c;
+    public static final int tab_menu_deal=0x7f02000d;
+    public static final int tab_menu_more=0x7f02000e;
+    public static final int tab_menu_poi=0x7f02000f;
+    public static final int tab_menu_text=0x7f020010;
+    public static final int tab_menu_user=0x7f020011;
   }
   public static final class id {
-    public static final int progressBar1=0x7f020000;
-    public static final int textView1=0x7f020001;
-    public static final int tv_splash_version=0x7f020002;
+    public static final int btn_four=0x7f030000;
+    public static final int btn_one=0x7f030001;
+    public static final int btn_three=0x7f030002;
+    public static final int btn_two=0x7f030003;
+    public static final int div_tab_bar=0x7f030004;
+    public static final int fragment_container=0x7f030005;
+    public static final int ly_tab_menu=0x7f030006;
+    public static final int ly_tab_menu_deal=0x7f030007;
+    public static final int ly_tab_menu_more=0x7f030008;
+    public static final int ly_tab_menu_poi=0x7f030009;
+    public static final int ly_tab_menu_user=0x7f03000a;
+    public static final int ly_top_bar=0x7f03000b;
+    public static final int progressBar1=0x7f03000c;
+    public static final int tab_menu_deal=0x7f03000d;
+    public static final int tab_menu_deal_num=0x7f03000e;
+    public static final int tab_menu_more=0x7f03000f;
+    public static final int tab_menu_more_num=0x7f030010;
+    public static final int tab_menu_poi=0x7f030011;
+    public static final int tab_menu_poi_num=0x7f030012;
+    public static final int tab_menu_setting_partner=0x7f030013;
+    public static final int tab_menu_user=0x7f030014;
+    public static final int tv_splash_version=0x7f030015;
+    public static final int txt_topbar=0x7f030016;
   }
   public static final class layout {
-    public static final int activity_home=0x7f030000;
-    public static final int activity_splash=0x7f030001;
+    public static final int activity_home=0x7f040000;
+    public static final int activity_splash=0x7f040001;
+    public static final int fragment_my=0x7f040002;
   }
   public static final class string {
-    public static final int app_name=0x7f040000;
+    public static final int app_name=0x7f050000;
   }
   public static final class style {
-    public static final int AppBaseTheme=0x7f050000;
-    public static final int AppTheme=0x7f050001;
+    public static final int AppBaseTheme=0x7f060000;
+    public static final int AppTheme=0x7f060001;
+    public static final int tab_menu_bgnum=0x7f060002;
+    public static final int tab_menu_text=0x7f060003;
   }
 }
